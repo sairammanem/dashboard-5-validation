edc9790433850974a77ef336031f5775b273d73e
sairammanem <sairammanem@gmail.com>
2021-05-03 15:34:42 +0530

    alt changed

--- src/topbar.js
+++ src/topbar.js
@@ -173,7 +173,7 @@ export default function Topbar(){
             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
             <img class="img-profile rounded-circle"
-                src="img/undraw_profile.svg"/>
+                src="img/undraw_profile.svg" alt="..."/>
         </a>
         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
             aria-labelledby="userDropdown">
